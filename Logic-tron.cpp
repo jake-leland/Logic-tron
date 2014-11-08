39e62abb944d0c23966c5b410606628660e95c1d
Jake Leland <jake.leland@att.net>
2014-11-08 15:04:15 -0600

    Updated save/load functions

--- Logic-tron.cpp
+++ Logic-tron.cpp
@@ -6,19 +6,41 @@
 
 #include "Gate.h"
 
-void save() {
+Vector<Gate*> gate_pointers;
+
+void save(String filename) {
+    ofstream ofs(filename.c_str());
+    // throw an error if the file is unable to be opened
+    if (!ofs) error("can’t open output file ", filename);
+    
+    for (auto g : gate_pointers) {
+        ofs << *g;
+    }
+    
     return;
 }
 
-void load() {
+void load(String filename) {
+    ifstream ifs(filename.c_str());
+    // throw an error if the file is unable to be opened
+    if (!ifs) error("can’t open input file ", filename);
+    
+    while(!ifs.eof()) {
+        Gate g;
+        ifs >> g;
+        if(ifs)
+            gate_pointers.push_back(&g);
+        else
+            error("problem reading file");
+    }
+    
     return;
 }
 
 int main() {
     try { // attempt the program
         if(H112 != 201401L)error("Error: incorrect std_lib_facilities_4.h version ", H112);
-        
-        Vector_ref<Gate> gates;
+
         
         return 0;
     } catch (Gate::Invalid) {
