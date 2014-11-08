47515cb2d973c951f89220aa027b7985591c06d8
Jake Leland <jake.leland@att.net>
2014-11-08 17:07:48 -0600

    GUI Progress
    Doesn’t exactly work yet.

--- Logic_GUI.cpp
+++ Logic_GUI.cpp
@@ -1,7 +1,24 @@
 // Logic_GUI.cpp
 
+#include "Gate.h"
 #include "Logic_GUI.h"
 
-void create_window() {
-    Simple_window win(Point(100,100),1024,768,"Logic-tron");
+
+Logic_GUI::Logic_GUI(int w, int h)
+: win_w(w), win_h(h), win(Point(100,100), win_w, win_h,"Logic-tron") {
+    Lines input_lines;
+    input_lines.add(Point(0,100), Point(win_w,100));
+    input_lines.set_color(Color::black);
+    win.attach(input_lines);
+    win.redraw();
+    win.wait_for_button();
+}
+
+void Logic_GUI::add_gate() {
+    // test to see if it works
+    Circle c(Point(100,100),100);
+    c.set_color(Color::black);
+    win.attach(c);
+    win.redraw();
+    win.wait_for_button(); // this causes an error
 }
--- Logic_GUI.h
+++ Logic_GUI.h
@@ -3,3 +3,14 @@
 #include "std_lib_facilities_4.h"
 #include "Simple_window.h"
 #include "Graph.h"
+
+class Logic_GUI {
+    int win_w;
+    int win_h;
+    Simple_window win;
+    
+public:
+    Logic_GUI(int w = 1024, int h = 768);
+    
+    void add_gate();
+};
