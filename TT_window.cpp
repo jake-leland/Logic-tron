efd26950415335945ed0a0c2c3aaf257e0302347
cbub <cassiebub@dhcp-10-202-117-153.tamulink.tamu.edu>
2014-12-02 18:09:52 -0600

    Started Desired Truth Table

--- TT_window.cpp
+++ TT_window.cpp
@@ -6,17 +6,19 @@
 
 
 TT_window::TT_window(int w, int h) : Window(Point(1024,0), w, h, "Truth Table"),
-l(Point(15,39),Point(535,39)) {
+l(Point(15,39),Point(590,39)) {
     l.set_color(Color::white);
     l.set_style(Line_style::dash);
     redraw();
+    String s = "00000000";
+    make_desired_table(s);
 }
 
 // function accepts a gate pointer, then retrieves the truth table information from that gate
 // a new column is automatically added that displays this truth table
 void TT_window::add_column(Gate* g) {
     
-    rects.push_back(new Rectangle(Point(15+20*rects.size(),15),20,190));
+    rects.push_back(new Rectangle(Point(15+20*(rects.size()-1),15),20,190));
     rects[rects.size()-1]->set_color(Color::black);
     
     if(rects.size()%2 == 0) {
@@ -26,7 +28,7 @@ void TT_window::add_column(Gate* g) {
     }
     
     attach(*rects[rects.size()-1]); // background color
-    headers.push_back(new Text(Point(21+20*headers.size(),32),to_string(g->getPosition())));
+    headers.push_back(new Text(Point(21+20*(headers.size()-1),32),to_string(g->getPosition())));
     headers[headers.size()-1]->set_color(Color::white);
     attach(*headers[headers.size()-1]);
     
@@ -35,7 +37,7 @@ void TT_window::add_column(Gate* g) {
     
     for(bool b : table) {
         String b_str(b?"1":"0"); // converts boolean to "0" or "1"
-        columns[columns.size()-1].push_back(new Text(Point(21+20*(columns.size()-1),56+20*columns[columns.size()-1].size()),b_str));
+        columns[columns.size()-1].push_back(new Text(Point(21+20*(columns.size()-2),56+20*columns[columns.size()-1].size()-1),b_str));
         columns[columns.size()-1][columns[columns.size()-1].size()-1]->set_color(Color::white);
         attach(*columns[columns.size()-1][columns[columns.size()-1].size()-1]);
     }
@@ -68,7 +70,37 @@ void TT_window::rm_column(){
 	delete (*it);
     }
     columns.pop_back();
-    redraw();
+}
+
+bool TT_window::match_truth_table(Gate* g){
+    Vector<bool> table = g->getTable();
+}
+
+void TT_window::update_desired_table(String s){
+    rects.push_back(new Rectangle(Point(550,15),40,190));
+    rects[rects.size()-1]->set_color(Color::black);
+    rects[rects.size()-1]->set_fill_color(Color::magenta);
+    
+    attach(*rects[rects.size()-1]); // background color
+    headers.push_back(new Text(Point(555,32),"Goal"));
+    headers[headers.size()-1]->set_color(Color::white);
+    attach(*headers[headers.size()-1]);
+    
+	Vector<bool> table;
+	for(int i = 0; i<s.size(); ++i) {
+		String sub = s.substr(i,1);
+		if(sub=="0") table.push_back(false);
+		else table.push_back(true);
+	}
+
+	columns.push_back(vector<Text*>());
+    for(bool b : table) {
+        String b_str(b?"1":"0"); // converts boolean to "0" or "1"
+        columns[columns.size()-1].push_back(new Text(Point(560,56+20*columns[columns.size()-1].size()),b_str));
+        columns[columns.size()-1][columns[columns.size()-1].size()-1]->set_color(Color::white);
+        attach(*columns[columns.size()-1][columns[columns.size()-1].size()-1]);
+    }
+
 }
 
 TT_window::~TT_window(){
--- TT_window.h
+++ TT_window.h
@@ -12,11 +12,14 @@ class TT_window : Graph_lib::Window{
     vector<Rectangle*> rects;
     vector<Text*> headers;
     vector< vector<Text*> > columns;
+    vector<bool> desiredTable;
     Line l;
 public:
-    TT_window(int w = 550, int h = 220);
+    TT_window(int w = 700, int h = 300);
 
     void add_column(Gate*);
     void rm_column();
+    bool match_truth_table(Gate*);
+    void update_desired_table(String s);
     ~TT_window();
 };
